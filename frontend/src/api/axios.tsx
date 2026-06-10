da3a2db2aa304bbf0d4ee2f1c8e8c33f00174a33
A06-Ahmed <atoubiahmed0@gmail.com>
2026-06-10 21:22:44 +0100

    fix: skip axios

--- frontend/src/api/axios.tsx
+++ frontend/src/api/axios.tsx
@@ -2,8 +2,12 @@ import { store } from "@/redux/store";
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: `${import.meta.env.VITE_API_URL}/api/`,
+  baseURL: `${import.meta.env.VITE_API_URL}/api/`,  // This assumes VITE_API_URL doesn't include /api
   withCredentials: true,
+  headers: {
+    'Content-Type': 'application/json',
+    'Accept': 'application/json',
+  }
 });
 
 api.interceptors.request.use(
