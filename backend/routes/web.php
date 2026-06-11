f52724ce4b9fd769ec96ccadbc3cbe63e3c15974
A06 <atoubiahmed0@gmail.com>
2026-06-11 07:51:43 +0100

    Update web.php

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -3,3 +3,10 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {return ['laravel' => app()->version()];});
+
+
+Route::get('/health', function () {
+    return response()->json([
+        'status' => 'ok'
+    ]);
+});
